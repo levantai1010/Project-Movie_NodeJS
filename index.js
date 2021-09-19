d520f004aab97d0ee7986e69a1cb76f51d4857c3
levantai1010 <75655478+levantai1010@users.noreply.github.com>
2021-09-19 20:22:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/graphql", graphqlHTTP({
     graphiql: true, // bat tool de dung
 }))
 // setup static file
-app.use("/public", express.static("public"));
+//app.use("/public", express.static("public"));
 
 // setup static file
 const pathPublicDirectory = path.join(__dirname, "./public");
